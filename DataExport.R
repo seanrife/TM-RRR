07ba529511b1085e12cfda7e760835bb63aea6c9
Sean C. Rife <seanrife@gmail.com>
2017-06-29 14:53:19 -0500

    Initial commit of analyses and export scripts

---
+++ DataExport.R
@@ -0,0 +1,25 @@
+# Data export for RRR of Trafimow and Hughes (2012), Study 3
+# Sean C. Rife / @seanrife / seanrife.com / srife1@murraystate.edu
+
+# Clean house
+rm(list = ls())
+
+
+# Function to read in and clean datafiles from each lab
+readInFile <- function(filename) {
+  df <- read.csv(filename, header = T, stringsAsFactors = F, check.names=F)
+  names(df) <- gsub(x = names(df),
+                    pattern = "\\]",
+                    replacement = "")
+  names(df) <- gsub(x = names(df),
+                    pattern = "\\[",
+                    replacement = "_")
+  names(df) <- gsub(x = names(df),
+                    pattern = "\\_SQ001",
+                    replacement = "")
+  df <- df[, -c(which(colnames(df)=="DEBRIEFING"):which(colnames(df)=="WTDP2Time"))]
+  df <- df[, -c(which(colnames(df)=="ARTICLETime"):which(colnames(df)=="DEBRIEFINGTime"))]
+  return(df)
+}
+
+df <- readInFile("C:\\Users\\srife1\\Dropbox\\Research\\TM RRR\\Datasets\\survey_357664_R_data_file.csv")
