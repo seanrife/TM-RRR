f5d046b6e3c9d4169e96f67b5bd7b31b22893b2f
Sean C. Rife <seanrife@gmail.com>
2021-08-22 16:34:42 -0500

    Script to export data for coding

--- DataExport.R
+++ DataExport.R
@@ -6,29 +6,30 @@ rm(list = ls())
 
 
 ##### USER DEFINED #####
-dataDir <- ""
+baseDir <- "H:\\Dropbox\\Research\\TM RRR" # DESKTOP
+#baseDir <- "C:\\Users\\srife1\\Dropbox\\Research\\TM RRR" # LAPTOP
+
+dataDir <- paste0(baseDir, "\\data")
 
 
 # Function to read in and clean datafiles from each lab
 readInFile <- function(filename) {
+  varNamesToRetain <- c("id", "Purpose")
   df <- read.csv(filename, header = T, stringsAsFactors = F, check.names=F)
-  names(df) <- gsub(x = names(df),
-                    pattern = "\\]",
-                    replacement = "")
-  names(df) <- gsub(x = names(df),
-                    pattern = "\\[",
-                    replacement = "_")
-  names(df) <- gsub(x = names(df),
-                    pattern = "\\_SQ001",
-                    replacement = "")
-  df <- df[, c(which(colnames(df)=="id"), which(colnames(df)=="labID"), which(colnames(df)=="WGTASK_word1"):which(colnames(df)=="WGTASK_word5"))]
+  colnames(df)[1] <- "id"
+  df <- df[varNamesToRetain]
   return(df)
 }
 
-files <- list.files(path=dataDir, pattern="*.csv", full.names=T, recursive=F)
 
-lapply(files, function(x) {
-  df <- readInFile(x)
-  write.table(df, paste0(dataDir,"/out.csv"), sep="\t", quote=F, row.names=F, col.names=T, append=T)
-})
+labInfo <- read.csv(paste0(dataDir,"\\LabInfo.csv"), stringsAsFactors=FALSE)
+labNames <- as.vector(labInfo$labID)
+
+for (labName in labNames) {
+  labData <- readInFile(paste0(dataDir, '\\', labName, '_main.csv'))
+  labData <- labData[!apply(labData, 1, function(x) any(x=="")),]
+  labData[,"Decision"] <- ""
+  write.csv(labData, paste0(dataDir, '\\', labName, '_coding.csv'), row.names = F)
+}
+
 
